841c0388aa5c6d90d23285f0ba5e6aca1423aae4
p6nj <breee@duck.com>
2023-02-26 18:37:20 +0100

    Works but not efficiently

---
+++ src/auto-setcap.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+setcap cap_net_raw,cap_net_admin=eip target/debug/testing
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 use openrgb::OpenRGB;
+use pcap::Capture;
 use rgb::RGB;
 use std::error::Error;
-mod nettest;
 
-// #[tokio::main]
-// async
-fn main() -> Result<(), Box<dyn Error>> {
-    // let client = OpenRGB::connect().await?;
-    // client.set_name("Rust").await?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = OpenRGB::connect().await?;
+    client.set_name("Rust").await?;
 
-    // client.update_led(0, 0, RGB { r: 255, g: 0, b: 0 }).await?;
+    let led_number = client.get_controller(0).await?.leds.len();
 
-    nettest::main();
+    let mut cap = Capture::from_device("wlo1")
+        .unwrap()
+        .immediate_mode(true)
+        .open()
+        .unwrap();
+
+    while let Ok(packet) = cap.next_packet() {
+        let size = packet.data.len();
+        for i in 0..match size * 3 < led_number * 3 {
+            true => size * 3,
+            false => led_number * 3,
+        } {
+            client
+                .update_led(
+                    0,
+                    i.try_into().unwrap(),
+                    RGB {
+                        r: packet.data[i],
+                        g: packet.data[i + 1],
+                        b: packet.data[i + 2],
+                    },
+                )
+                .await?;
+        }
+    }
 
     Ok(())
 }
